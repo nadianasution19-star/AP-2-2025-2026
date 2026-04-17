dac303c57c0e7f5a1c26070aa7f47768392b02da
nadianasution19-star <nadianasution19@gmail.com>
2026-04-17 16:56:35 +0700

    PERTEMUAN 2 : PENGGUNAAN OPERATOR

---
+++ Pertemuan-2---Penggunaan-Operator/latihan.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    float r, volume, luas;
+const float pi = 3.14;
+
+cout << "Masukkan jari-jari : ";
+cin >> r;
+
+volume = (float)4 / (float)3 * pi * r * r * r;
+luas = 4 *pi * r* r;
+
+cout << " Volume Bola = " << volume << endl;
+cout << "Luas Bola = " << luas << endl;
+
+}
